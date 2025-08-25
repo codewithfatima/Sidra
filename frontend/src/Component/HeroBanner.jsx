12ed2627fd47fa25d3631d8e57f67f9721e5f0ec
Fatima <fatima68.05.07@gmail.com>
2025-08-25 10:04:33 +0300

    main

--- frontend/src/Component/HeroBanner.jsx
+++ frontend/src/Component/HeroBanner.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import Pic1 from '../../src/assets/Pic1.gif';
+import Pic1 from '../../src/assets/Pic1.jpg';
 import Pic2 from '../../src/assets/Pic22.gif';
 
 const slides = [
   { image: Pic1, titleKey: 'hero.slide1', link: 'https://www.happylandkw.com/' },
-  { image: Pic2, titleKey: 'hero.slide2', link:'https://mynurserykw.com/' },
+  { image: Pic2, titleKey: 'hero.slide2', link: 'https://mynurserykw.com/' },
 ];
 
 const HeroBanner = () => {
@@ -24,16 +24,17 @@ const HeroBanner = () => {
 
   return (
     <div className="w-full relative overflow-hidden mt-17">
-      {/* Responsive Image */}
-      <a href={slides[current].link} target='_blank' rel="noopener noreferrer">
-        <img
-          src={slides[current].image}
-          alt={`Slide ${current + 1}`}
-          className="cursor-pointer w-full h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[75vh] object-cover
-         transition-opacity duration-700"
-        />
 
+      <a href={slides[current].link} target="_blank" rel="noopener noreferrer">
+        <div className="w-full flex justify-center ">
+          <img
+            src={slides[current].image}
+            alt={`Slide ${current + 1}`}
+            className="w-full max-h-[90vh] object-contain transition-opacity duration-700"
+          />
+        </div>
       </a>
+
 
       {/* Dots */}
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
